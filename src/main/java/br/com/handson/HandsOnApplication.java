708f47fce28b8d157c29da02e5049f2c761e9a6b
Wagner Monteiro A Santos <wagner@engsolutions.com.br>
2018-02-05 16:22:22 -0200

    Commit Intellij

---
+++ src/main/java/br/com/handson/HandsOnApplication.java
@@ -0,0 +1,12 @@
+package br.com.handson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOnApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/handson/HandsOnApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.handson;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HandsOnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
