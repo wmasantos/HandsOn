bfe9abb2d8b1cc80af4bf6a469529add8771325d
Wagner Monteiro A Santos <wagnercct2@gmail.com>
2018-02-05 22:08:53 -0200

    H2 Connection

--- src/main/java/br/com/handson/HandsOnApplication.java
+++ src/main/java/br/com/handson/HandsOnApplication.java
@@ -2,11 +2,28 @@ package br.com.handson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @SpringBootApplication
-public class HandsOnApplication {
+@RestController
+public class HandsOnApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HandsOnApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HandsOnApplication.class);
+	}
+
+	@RequestMapping(value = "/")
+	public String healthCheck(){
+		return "API ROCK's - " + new Date().toString();
 	}
 }
---
+++ src/main/java/br/com/handson/util/H2Connection.java
@@ -0,0 +1,53 @@
+package br.com.handson.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class H2Connection {
+    private final String driver = "org.h2.Driver";
+    private final String url = "jdbc:h2:~/handson";
+    private final String usuario = "sa";
+    private final String senha = "";
+    private Connection con;
+
+    public H2Connection() {
+        try {
+            Class.forName(driver);            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(H2Connection.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+    }  
+    
+    public Connection getConnection(){
+        if(con == null){
+            try {
+                con = DriverManager.getConnection(url, usuario, senha);
+                con.setAutoCommit(false);
+                System.out.println("CONECTOU");
+                return con;
+            } catch (SQLException ex) {
+                Logger.getLogger(H2Connection.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        else{
+           return con; 
+        }
+        
+        return null;
+    }
+    
+    public boolean closeConnection(){
+        try {
+            con.close();
+            con = null;
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(H2Connection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+}
