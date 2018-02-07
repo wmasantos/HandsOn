f2db25276afcfadcc1185496ab422b7197f42cb5
Wagner Monteiro A Santos <wagner@engsolutions.com.br>
2018-02-07 13:56:10 -0200

    Commit regra de 30 minutos mínimo entre partidas

--- src/main/java/br/com/handson/repository/CompetitionRepository.java
+++ src/main/java/br/com/handson/repository/CompetitionRepository.java
@@ -1,12 +1,17 @@
 package br.com.handson.repository;
 
 import br.com.handson.entity.CompetitionEntity;
+import br.com.handson.entity.DatasEntity;
 import br.com.handson.util.H2Connection;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class CompetitionRepository {
     H2Connection con;
@@ -15,11 +20,36 @@ public class CompetitionRepository {
         con = new H2Connection();
     }
 
+    public boolean add(CompetitionEntity competitionEntity){
+        String sql = "INSERT INTO JOGO(IDFEDERACAO1, IDFEDERACAO2, IDETAPA, IDESTADIO, IDMODALIDADE, DATA_INICIO, DATA_TERMINO)VALUES(?, ?, ?, ?, ?, ?, ?);";
+
+        try {
+            PreparedStatement stm = con.getConnection().prepareCall(sql);
+            stm.setInt(1, competitionEntity.getFedaration1Id());
+            stm.setInt(2, competitionEntity.getFederation2Id());
+            stm.setInt(3, competitionEntity.getStepId());
+            stm.setInt(4, competitionEntity.getStadiumId());
+            stm.setInt(5, competitionEntity.getModalityId());
+            stm.setString(6, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(competitionEntity.getStartDate().getTime()));
+            stm.setString(7,  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(competitionEntity.getFinalDate().getTime()));
+
+            int result = stm.executeUpdate();
+            con.getConnection().commit();
+            con.closeConnection();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
     //Validação para saber se um jogo que será cadastrado não está sendo inserido no mesmo período, no mesmo local, para a
     //mesma modalidade. Ex: Se eu tenho uma partida de futebol que com início às 18:00 e
     //término às 20:00 no Estádio 1, eu não poderia ter outra partida de futebol se iniciando
     //às 19:30 nesse mesmo estádio
-    //
+
+    //OK
     public boolean verifyPlays(CompetitionEntity competitionEntity){
         String sql = "SELECT * FROM JOGO j INNER JOIN MODALIDADE m ON m.ID = j.IDMODALIDADE INNER JOIN ESTADIO e ON " +
                 "e.ID = j.IDESTADIO WHERE j.IDESTADIO = ? AND j.IDMODALIDADE = ? AND ((j.DATA_INICIO BETWEEN ? " +
@@ -30,12 +60,12 @@ public class CompetitionRepository {
             PreparedStatement stm = con.getConnection().prepareCall(sql);
             stm.setInt(1, competitionEntity.getStadiumId());
             stm.setInt(2, competitionEntity.getModalityId());
-            stm.setDate(3, new Date(competitionEntity.getStartDate().getTime()));
-            stm.setDate(4, new Date(competitionEntity.getFinalDate().getTime()));
-            stm.setDate(5, new Date(competitionEntity.getStartDate().getTime()));
-            stm.setDate(6, new Date(competitionEntity.getFinalDate().getTime()));
-            stm.setDate(7, new Date(competitionEntity.getStartDate().getTime()));
-            stm.setDate(8, new Date(competitionEntity.getFinalDate().getTime()));
+            stm.setString(3, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(competitionEntity.getStartDate()));
+            stm.setString(4, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(competitionEntity.getFinalDate().getTime()));
+            stm.setString(5, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(competitionEntity.getStartDate().getTime()));
+            stm.setString(6, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(competitionEntity.getFinalDate().getTime()));
+            stm.setString(7, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(competitionEntity.getStartDate().getTime()));
+            stm.setString(8, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(competitionEntity.getFinalDate().getTime()));
 
             ResultSet rs = stm.executeQuery();
             return !rs.next();
@@ -44,6 +74,25 @@ public class CompetitionRepository {
         }
 
         return false;
+    }
+
+    public List<DatasEntity> getDados(){
+        String sql = "SELECT j.DATA_INICIO, j.DATA_TERMINO FROM JOGO j";
+
+        try {
+            PreparedStatement stm = con.getConnection().prepareCall(sql);
+            ResultSet rs = stm.executeQuery();
+            List<DatasEntity> datasEntities = new ArrayList<>();
+
+            while(rs.next())
+                datasEntities.add(new DatasEntity(rs.getString(1), rs.getString(2)));
+
+            return datasEntities;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     /*
@@ -56,8 +105,13 @@ public class CompetitionRepository {
     }
 
     //A competição deve ter a duração de no mínimo 30 minutos.
-    public String verifyTime(){
-        return null;
+    public boolean verifyTime(CompetitionEntity competitionEntity){
+        Calendar checkAux = Calendar.getInstance();
+        checkAux.setTime(competitionEntity.getStartDate());
+
+        checkAux.add(Calendar.MINUTE, 30);
+
+        return competitionEntity.getFinalDate().after(checkAux.getTime());
     }
 
     //Para evitar problemas, a organização das olimpíadas que limitar a no máximo 4
--- src/main/java/br/com/handson/resource/CheckListResource.java
+++ src/main/java/br/com/handson/resource/CheckListResource.java
@@ -2,24 +2,35 @@ package br.com.handson.resource;
 
 import br.com.handson.entity.CheckListEntity;
 import br.com.handson.entity.CompetitionEntity;
+import br.com.handson.entity.DatasEntity;
 import br.com.handson.entity.ResultWithContentEntity;
 import br.com.handson.repository.CompetitionRepository;
 import br.com.handson.service.CheckListService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/checkList")
 public class CheckListResource {
-    @GetMapping
+    /*@GetMapping
     public ResultWithContentEntity<CheckListEntity> check(){
         CheckListService checkListService = new CheckListService();
 
         return checkListService.checkList();
+    }*/
+
+    @GetMapping
+    public List<DatasEntity> getDados(){
+        return new CompetitionRepository().getDados();
     }
 
     @PostMapping
     public String test(@RequestBody CompetitionEntity competitionEntity){
-        return String.valueOf(new CompetitionRepository().verifyPlays(competitionEntity));
+        boolean teste = new CompetitionRepository().verifyPlays(competitionEntity);
+        boolean res = new CompetitionRepository().add(competitionEntity);
+        boolean res2 = new CompetitionRepository().verifyTime(competitionEntity);
+        return "";
     }
 }
